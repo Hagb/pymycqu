f2a25d8e2ec0e876aebb00ecbe76615b7b000c85
Hagb Green <hagb_green@qq.com>
2021-12-02 15:16:18 +0800

    Fix mycqu login

--- mycqu/mycqu.py
+++ mycqu/mycqu.py
@@ -9,7 +9,7 @@ __all__ = ("access_mycqu",)
 MYCQU_TOKEN_INDEX_URL = "https://my.cqu.edu.cn/enroll/token-index"
 MYCQU_TOKEN_URL = "https://my.cqu.edu.cn/authserver/oauth/token"
 MYCQU_AUTHORIZE_URL = f"https://my.cqu.edu.cn/authserver/oauth/authorize?client_id=enroll-prod&response_type=code&scope=all&state=&redirect_uri={MYCQU_TOKEN_INDEX_URL}"
-MYCQU_SERVICE_URL = "https://my.cqu.edu.cn/authserver/authentication/cas"
+MYCQU_SERVICE_URL = "http://my.cqu.edu.cn/authserver/authentication/cas"
 CODE_RE = re.compile(r"\?code=([^&]+)&")
 
 
@@ -17,8 +17,9 @@ class MycquUnauthorized(Exception):
     def __init__(self):
         super().__init__("Unanthorized in mycqu, auth.login firstly and then mycqu.access_mycqu")
 
-# from https://github.com/CQULHW/CQUQueryGrade
+
 def get_oauth_token(session: Session) -> str:
+    # from https://github.com/CQULHW/CQUQueryGrade
     resp = session.get(MYCQU_AUTHORIZE_URL, allow_redirects=False)
     assert (match := CODE_RE.search(resp.headers['Location']))
     token_data = {
@@ -42,7 +43,8 @@ def access_mycqu(session: Session, add_to_header: bool = True) -> Dict[str, str]
     :return: mycqu 认证信息的请求头，当 ``add_to_header`` 参数为 :obj:`True` 时无需手动使用该返回值
     :rtype: Dict[str, str]
     """
-    access_service(session, MYCQU_SERVICE_URL)
+    session.get(access_service(session, MYCQU_SERVICE_URL).headers['Location'],
+                allow_redirects=False)  # http 302 to https
     token = get_oauth_token(session)
     if add_to_header:
         session.headers["Authorization"] = token
