5fca3d044e44d091f680e0d5a1f2aa6874689818
Hagb <hagb_green@qq.com>
2021-12-12 22:52:09 +0800

    Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Hagb'
 author = 'Hagb'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.2.3'
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
