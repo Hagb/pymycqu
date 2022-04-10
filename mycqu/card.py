aed75cd4af6b8308a0b07333e1928c96787d856d
Zhu Zijun <2961163526@qq.com>
2022-04-10 23:24:50 +0800

    为老校区水电费查询提供了支持

--- mycqu/card.py
+++ mycqu/card.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 
 import json
 import datetime
-from typing import Any, List
+from typing import Any, List, Optional
 from html.parser import HTMLParser
 import requests
 from requests import Session
@@ -24,14 +24,14 @@ FEE_ITEM_ID = {'Huxi': '182',
 LOGIN_URL = 'http://card.cqu.edu.cn:7280/ias/prelogin?sysid=FWDT'
 
 
-def get_fees_raw(session: Session, isHuxi: bool, room: str):
+def get_fees_raw(session: Session, is_huxi: bool, room: str):
     """
     从card.cqu.edu.cn获取水电费详情
 
     :param session: 登录了统一身份认证（:func:`.auth.login`）并在 card.cqu.edu.cn 进行了认证（:func:`.card.access_card`）的 requests 会话
     :type session: Session
-    :param isHuxi: 房间号是否为虎溪校区的房间
-    :type isHuxi: bool
+    :param is_huxi: 房间号是否为虎溪校区的房间
+    :type is_huxi: bool
     :param room: 需要获取水电费详情的宿舍
     :type room: str
     :raises NetworkError: 当访问相关网页时statue code不为200时抛出
@@ -43,7 +43,7 @@ def get_fees_raw(session: Session, isHuxi: bool, room: str):
     """
     ticket = _get_ticket(session)
     synjones_auth = _get_synjones_auth(ticket)
-    return get_fee_data(synjones_auth, room, FEE_ITEM_ID['Huxi'] if isHuxi else FEE_ITEM_ID['Old'])
+    return get_fee_data(synjones_auth, room, FEE_ITEM_ID['Huxi'] if is_huxi else FEE_ITEM_ID['Old'])
 
 
 def get_card_raw(session):
@@ -149,32 +149,49 @@ class EnergyFees:
     """
     balance: float
     """账户余额"""
-    electricity_subsidy: float
-    """电剩余补助"""
-    water_subsidy: float
-    """水剩余补助"""
+    electricity_subsidy: Optional[float]
+    """电剩余补助（仅虎溪校区拥有）"""
+    water_subsidy: Optional[float]
+    """水剩余补助（仅虎溪校区拥有）"""
+    subsidies: Optional[float]
+    """补助余额（仅老校区拥有）"""
 
     @staticmethod
-    def from_dict(data: dict[str, Any]) -> EnergyFees:
+    def from_dict(data: dict[str, Any], is_huxi: bool) -> EnergyFees:
         """从反序列化的（一个）水电费 json 中获取水电费信息
 
         :param data: json 反序列化得到的字典
         :type data: dict[str, Any]
+        :param is_huxi: 目标寝室是否在虎溪校区
+        :type is_huxi: bool
         :return: 学期信息对象
         :rtype: EnergyFees
         """
-        return EnergyFees(
-            balance=data["剩余金额"],
-            electricity_subsidy=data["电剩余补助"],
-            water_subsidy=data["水剩余补助"]
-        )
+        if is_huxi:
+            return EnergyFees(
+                balance=data["剩余金额"],
+                electricity_subsidy=data["电剩余补助"],
+                water_subsidy=data["水剩余补助"],
+                subsidies=None,
+            )
+        else:
+            return EnergyFees(
+                balance=data["现金余额"],
+                electricity_subsidy=None,
+                water_subsidy=None,
+                subsidies=data["补贴余额"],
+            )
 
     @staticmethod
-    def fetch(session: Session, isHuxi: bool, room: str) -> EnergyFees:
+    def fetch(session: Session, is_huxi: bool, room: str) -> EnergyFees:
         """从 card.cqu.edu.cn 上获取当前水电费信息，需要登录了统一身份认证的会话
 
-        :param session: 登录了统一身份认证（:func:`.auth.login`）的 requests 会话
+        :param session: 登录了统一身份认证（:func:`.auth.login`）并在 card.cqu.edu.cn 进行了认证（:func:`.card.access_card`）的 requests 会话
         :type session: Session
+        :param is_huxi: 房间号是否为虎溪校区的房间
+        :type is_huxi: bool
+        :param room: 需要获取水电费详情的宿舍
+        :type room: str
         :raises NetworkError: 当访问相关网页时statue code不为200时抛出
         :raises TicketGetError: 当未能从网页对应位置中获取到ticket时抛出
         :raises ParseError: 当从返回数据解析所需值失败时抛出
@@ -182,7 +199,7 @@ class EnergyFees:
         :return: 返回相关宿舍的水电费信息
         :rtype: EnergyFees
         """
-        return EnergyFees.from_dict(get_fees_raw(session, isHuxi, room)["map"]["showData"])
+        return EnergyFees.from_dict(get_fees_raw(session, is_huxi, room)["map"]["showData"], is_huxi)
 
 
 @dataclass
