fa112974f99030a1015315666d75a1671dc16d3a
Zhu Zijun <2961163526@qq.com>
2022-08-20 05:41:13 +0800

    Import `library` module in `__init__`

--- mycqu/__init__.py
+++ mycqu/__init__.py
@@ -1,16 +1,18 @@
-from . import auth, course, exam, mycqu, score, card, exception
+from . import auth, course, exam, mycqu, score, card, exception, library
 from .auth import *
 from .course import *
 from .exam import *
 from .score import *
 from .mycqu import *
 from .card import *
+from .library import *
 from .exception import *
-__all__ = ["auth", "course", "exam", "mycqu", "user", "score", "card"]
+__all__ = ["auth", "course", "exam", "mycqu", "user", "score", "card", "library"]
 __all__.extend(auth.__all__)
 __all__.extend(course.__all__)
 __all__.extend(exam.__all__)
 __all__.extend(score.__all__)
 __all__.extend(mycqu.__all__)
 __all__.extend(card.__all__)
+__all__.extend(library.__all__)
 __all__.extend(exception.__all__)
